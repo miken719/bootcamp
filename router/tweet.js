7d70d4a92bc3abc2b08df4bd73d0b79a2b252681
Miken Prajapati <miken.prajapati@indianic.com>
2023-08-17 16:55:40 +0530

    tweeet updation

--- router/tweet.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { tweet } = require("../controller/tweet");
-
-const router = express.Router();
-
-router.post("/tweet", tweet);
-
-module.exports = router;
